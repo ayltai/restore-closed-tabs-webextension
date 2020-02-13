c8db0b00810adbc600999132f6d9cba805cb4c1a
Alan Tai <ayltai@gmail.com>
2020-02-13 10:33:08 +0800

    Add unit tests; integrate React, Codacy, CodeCov, SonarCloud, CircleCI

---
+++ public/options/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const MAX_RESULTS      = 10;
+const MESSAGE_DURATION = 1500;
+
+document.addEventListener('DOMContentLoaded', () => browser.storage.sync.get({
+    maxResults : MAX_RESULTS,
+}, preferences => {
+    document.getElementById('maxResults').value = preferences.maxResults;
+}));
+
+document.getElementById('save').addEventListener('click', () => {
+    const maxResults = Number(document.getElementById('maxResults').value) || MAX_RESULTS;
+
+    browser.storage.sync.set({
+        maxResults : Math.min(browser.sessions.MAX_SESSION_RESULTS, Math.max(2, maxResults)),
+    }, () => {
+        const node = document.getElementById('message');
+        node.textContent = 'Settings saved';
+
+        window.setTimeout(() => node.textContent = '', MESSAGE_DURATION);
+    });
+});
---
+++ src/Utils.js
@@ -0,0 +1,25 @@
+export const getPreferences = (defaultValues, callback) => {
+    window.browser.storage.sync.get(defaultValues, preferences => callback(preferences));
+};
+
+export const getRecentlyClosedTabs = (limit, callback, getPrefs = getPreferences) => {
+    getPrefs({
+        maxResults : limit,
+    }, preferences => {
+        window.browser.sessions.getRecentlyClosed({
+            maxResults : preferences.maxResults,
+        }, sessions => {
+            const tabs = [];
+
+            sessions.slice(0, preferences.maxResults).forEach(session => {
+                if (session.tab) {
+                    tabs.push(session.tab);
+                } else if (session.window) {
+                    session.window.tabs.forEach(tab => tabs.push(tab));
+                }
+            });
+
+            callback(tabs);
+        });
+    });
+};

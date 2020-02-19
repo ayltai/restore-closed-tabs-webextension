b0040a02a0a4dcc60cd5d0fcb2e43b216ebd7773
Alan Tai <ayltai@gmail.com>
2020-02-19 10:44:32 +0800

    Add unit test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,14 +8,14 @@ import { App } from './App';
 window.browser = browser;
 
 describe('<App />', () => {
+    it('renders correctly', () => {
+        expect(renderer.create(<App />).toJSON()).toMatchSnapshot();
+    });
+
     it('renders without errors', () => {
         const div = document.createElement('div');
 
         ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
-    });
-
-    it('renders correctly', () => {
-        expect(renderer.create(<App />).toJSON()).toMatchSnapshot();
     });
 });
